c602a5875fb5c276d1e71220623ec190dd1c6dea
Tajuddin80 <tajuddin.cse.dev@gmail.com>
2026-02-04 17:09:45 +0600

    added js file

---
+++ assets/product-variant.js
@@ -0,0 +1,9 @@
+function updateSelection () {
+    let selectedValues = ""
+
+}
+const selectedOptions = document.querySelector(".product-options input[type='radio']")
+
+$('.product-options input[type="radio"]').change(updateSelection);
+
+updateSelection()
